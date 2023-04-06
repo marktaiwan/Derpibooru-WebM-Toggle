aa5559283b868b580c80758245385b4ad46969f7
Mark Taiwan <marktaiwan@users.noreply.github.com>
2023-04-06 12:25:26 +0800

    Version 1.4.7

--- webm-volume-toggle.user.js
+++ webm-volume-toggle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Derpibooru WebM Volume Toggle
 // @description  Audio toggle for WebM clips
-// @version      1.4.5
+// @version      1.4.7
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
